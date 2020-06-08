2b4f5be3c5f3bdc9b3fc04166cb5c247e6c5e038
ddincho <dianelf.1106@gmail.com>
2020-06-08 12:20:10 +0900

    Ionic Storage dan Database SQLite

---
+++ JOB 3/src/pages/menu/menu.ts
@@ -0,0 +1,90 @@
+import { Component, ViewChild } from '@angular/core'; 
+import { Platform, Nav, NavController, NavParams, AlertController } from 'ionic-angular'; 
+import { StatusBar } from '@ionic-native/status-bar'; 
+ 
+import { HomePage } from '../home/home'; 
+import { AboutPage } from '../about/about'; 
+import { UserAccount } from '../../models/user/user-model'; 
+import { LoginPage } from '../login/login'; 
+import { DatabaseProvider } from '../../providers/database/database'; 
+import { CategoryPage } from '../category/category';
+/**
+ * Generated class for the MenuPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */ 
+ 
+@Component({   
+  selector: 'page-menu',   
+  templateUrl: 'menu.html', 
+}) 
+
+export class MenuPage { 
+ 
+  @ViewChild(Nav) nav: Nav; 
+ 
+  pages: Array<{title: string, component: any}>;      
+  userAccount: UserAccount = <UserAccount>{}; 
+ 
+  constructor(public platform: Platform, public statusBar: StatusBar, public navCtrl: NavController, public navParams: NavParams, public alerCtrl: AlertController, public databaseProvider: DatabaseProvider) {     this.initializeApp();     
+    this.userAccount = this.navParams.get("account");     
+    if(this.userAccount){       
+      console.log("userAccount Params available in MenuPage!");       
+      console.log(this.userAccount);     
+    }else{       
+      console.log("userAccount Params not Available in MenuPage!");     
+    }    
+      this.pages = [       
+        { title: 'Home', component: HomePage },       
+        { title: 'Manage Category', component: CategoryPage },       
+        { title: 'About Developer', component: AboutPage },     
+      ];   
+    } 
+ 
+  ionViewDidLoad() {     
+    console.log('ionViewDidLoad MenuPage');     
+    this.nav.push(HomePage, {menuAccount: this.userAccount});   
+  } 
+ 
+  initializeApp() {     
+    this.platform.ready().then(() => {       
+      this.statusBar.styleDefault(); 
+      this.databaseProvider.createDatabase()       
+      .then(() => {         
+        console.log("Success load database.");       
+      })       
+      .catch(() => {         
+        console.log("Failed load database."); 
+      });     
+    });   
+  } 
+ 
+  openPage(page) {    
+    this.nav.setRoot(page.component, {menuAccount: this.userAccount});   
+  } 
+ 
+  logout(){     
+    let confirm = this.alerCtrl.create({       
+      title: 'Confirm Logout',       
+      message: 'Do you really to logout?',       
+      buttons: [         
+        {           
+          text: 'No',           
+          handler: () => {             
+            console.log('No clicked');           
+          }         
+        },         
+        {           
+          text: 'Yes',           
+          handler: () => {             
+            console.log('Yes clicked');             
+            this.navCtrl.setRoot(LoginPage);           
+          }         
+        }       
+      ]     
+    });     
+    confirm.present()   
+  } 
+ 
+}
